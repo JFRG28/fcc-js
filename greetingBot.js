7c6ff278f603f8514fbe7ec22281411c520bf9b9
JFRG28 <fgrodriguez.28@gmail.com>
2026-01-07 11:20:25 -0600

    finished practice

--- greetingBot.js
+++ greetingBot.js
@@ -6,3 +6,24 @@ bot = "teacherBot";
 let botLocation = "the universe";
 
 console.log("Allow me to introduce myself.");
+let botIntroduction
+botIntroduction="My name is " + bot + ".";
+console.log(botIntroduction);
+let botLocationSentence;
+botLocationSentence="I live in " + botLocation + ".";
+console.log(botLocationSentence);
+bot="professorBot";
+
+let nicknameIntroduction;
+nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+bot="awesomeTeacherBot";
+let newNicknameGreeting;
+newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+let favoriteSubject;
+favoriteSubject="Computer Science";
+let favoriteSubjectSentence;
+favoriteSubjectSentence="My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+console.log("Well, it was nice to talk to you. Have a nice day!");
