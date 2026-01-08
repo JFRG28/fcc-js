ef83a0916e8cebf974edb82062b216c24aa3c3a6
JFRG28 <fgrodriguez.28@gmail.com>
2026-01-08 16:16:25 -0600

    truthy and falsy elements

---
+++ logicChecker.js
@@ -0,0 +1,20 @@
+let truthyOrFalsy = true;
+console.log(Boolean(truthyOrFalsy));
+truthyOrFalsy = "freeCodeCamp";
+console.log(Boolean(truthyOrFalsy));
+truthyOrFalsy = "";
+console.log(Boolean(truthyOrFalsy));
+let hasDeveloperJob = true
+if (hasDeveloperJob) {
+  console.log("Timmy is employed as a developer.");
+}
+let isTimmyAGamer = false;
+if (isTimmyAGamer) {
+  console.log("Timmy loves to play World of Warcraft.");
+}
+let timmyAge = 12;
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+} else {
+  console.log("Timmy is not old enough to drive.");
+}
